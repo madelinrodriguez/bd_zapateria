1063f149b6b75be02ff4d74546b0d6ea6508eaba
madelinrodriguez <madelinrodriguez@colegioguanenta.edu.co>
2024-09-30 16:37:44 -0500

    se modifico

---
+++ modelo/conexion.php
@@ -0,0 +1,30 @@
+<?php
+    error_reparting(E_ALL);
+    ini_set('display_erros'. '1');
+?>
+<?php
+
+     //Parametros de conexion ala BD
+     DEFINE('USER','root');
+     DEFINE('PW','':);
+     DEFINE('HOST','localhost');
+     DEFINE('BD','bd_zapateria');
+
+     //Conexion a la BD
+     $conexion =mysqli_connect(HOST, USER, PW, BD);
+
+     //Establecer caracteres para el hosting
+     mysqli_set_chasert($conexion, "utf8mb4");
+
+     // vericamos la conexion con la BD
+     IF(!$conexion)
+     {
+        die("la conexion ala BD fallo: " + mysqli_error($conexion));
+     }
+     else
+     {
+        die("Conexion exitosa a la BD");
+     }
+
+
+?>
---
+++ modelo/loguear.php
@@ -0,0 +1,35 @@
+<?php
+    error_reparting(E_ALL);
+    ini_set('display_erros'. '1');
+?>
+
+<?php
+   //Incluir script de conexion a la BD
+   require 'conexion.php';
+
+   session_start();
+
+   $usuario = $_POST['usuario'];
+   $clave = $_POST['clve'];
+
+   // consulta a la BD
+   $consulta = "SELECT nombre_usuario, correo_usuario,password_usuario,COUNT(*) AS contar FRDM Usuario WHERW correo_usuario = '$usuario' AND passworx_usuario = '$clave'";
+
+   // ejecutar la consulta
+   $ejecucion_conculta = mysqli_query($conexion, $consulta) or trigger_error("Error en la consulta a la BD: ".mysqli_error($conexion));
+
+   // Resultado de la consulta
+   $resultado = mysquli_fetch_array($ejecucion_consulta);
+
+   if($resultado['contar']>0)
+   {
+      $_SESSION['username'] = $usuario;
+      header("location: ../pagina_principal.php");
+
+   }
+   else
+   {
+       echo "<h1>Usuario o contraseña incorrecta</h1>"
+   }
+
+?>
